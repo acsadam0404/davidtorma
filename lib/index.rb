0ed9efc09259a45a96ec1dd819d582ee4d85d810
Ács Ádám <aacs@aacs-dt.rrintra.priv.rrsoftware.hu>
2014-06-13 10:00:24 +0200

    i18n

--- lib/index.rb
+++ lib/index.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
+require 'i18n'
+require 'i18n/backend/fallbacks'
+
+configure do
+  I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+ I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+  I18n.backend.load_translations
+  I18n.default_locale = :en
+end
 
 get '/' do
-   pics = Dir.entries(File.dirname(__FILE__) + "/public/images/thumbs").select {|f| !File.directory? f}
+    pics = Dir.entries(File.dirname(__FILE__) + "/public/images/thumbs").select {|f| !File.directory? f}
     erb :index, :locals => {:pics => pics} 
 end
 
@@ -9,4 +18,9 @@ post '/contact' do
 	contact =  params[:contact]
 	puts contact['name']
 	redirect '/#/contact'
+end
+
+post '/locale' do 
+	I18n.locale = params[:locale] || I18n.default_locale
+	redirect '/'
 end
